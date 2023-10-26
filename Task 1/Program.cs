78ca790a87f2c59a8dad5b2f1101c15a3d2acc1e
Aleksandr <146584991+GPraim@users.noreply.github.com>
2023-10-26 22:31:45 +0500

    Update Program.cs

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -26,6 +26,45 @@ double exponentiationNumber(int a, int b)
     return result;
 }
 
+/*int enterInteger(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[,] generateArray(int countCollums, int countLine, int a, int b){
+    int[,] array = new int[countLine, countCollums];
+    Random random = new Random();
+    for (int i = 0; i < countCollums; i++)
+    {
+        for (int m = 0; m < countLine; m++)
+        {
+            array[m, i] = random.Next(a, b);
+        }
+    }
+    return array;
+}
+
+void printMassive(int[,] collection){
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int m = 0; m < collection.GetLength(1); m++)
+        {
+            Console.Write($"{collection[i, m]} \t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int countCollums = enterInteger("Введите количество колоннок массива: ");
+int countLine = enterInteger("Введите количество строк массива: ");
+
+int[,] array = generateArray(countCollums, countLine, 1, 9);
+printMassive(array);*/
+
 int a = enterInteger("Введите число, которое будете возводить в степень: ");
 int b = enterInteger("Введите число, которое будет являться степенью: ");
 Console.WriteLine("Результат возведения в степень: " + exponentiationNumber(a, b));
